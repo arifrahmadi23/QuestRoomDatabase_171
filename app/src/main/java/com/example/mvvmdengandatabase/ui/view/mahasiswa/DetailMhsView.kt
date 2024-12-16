f74bb057e884513f185cf26b3e4fe28265785015
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-16 20:36:53 +0700

    Membuat fungsi BodyDetailMhs di DetailMhsView.kt

--- app/src/main/java/com/example/mvvmdengandatabase/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/mvvmdengandatabase/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,16 +1,22 @@
 package com.example.mvvmdengandatabase.ui.view.mahasiswa
 
+import android.content.ClipData.Item
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,6 +26,73 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
 import androidx.room.Delete
 import com.example.mvvmdengandatabase.data.entity.Mahasiswa
+import com.example.mvvmdengandatabase.ui.viewmodel.DetailUiState
+import com.example.mvvmdengandatabase.ui.viewmodel.toMahasiswaEntity
+
+@Composable
+fun BodyDetailMhs(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationRequired by remeberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center)
+            {
+                CircularProgressIndicator()
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ){
+                ItemDetailMhs(
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if(deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+
 
 @Composable
 fun ItemDetailMhs(
