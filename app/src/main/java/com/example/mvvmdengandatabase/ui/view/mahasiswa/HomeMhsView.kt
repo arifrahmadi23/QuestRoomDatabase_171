82d754e9671df824bbf888c55e41e131bd12df4b
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-16 19:27:18 +0700

    Membuat fungsi CardMhs untuk tampilan pada Home

---
+++ app/src/main/java/com/example/mvvmdengandatabase/ui/view/mahasiswa/HomeMhsView.kt
@@ -0,0 +1,16 @@
+package com.example.mvvmdengandatabase.ui.view.mahasiswa
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.mvvmdengandatabase.data.entity.Mahasiswa
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun CardMhs(
+    mhs: Mahasiswa,
+    modifier: Modifier = Modifier,
+    onClixk: () -> Unit = { }
+){
+
+}
