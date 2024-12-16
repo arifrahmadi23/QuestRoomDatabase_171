7f2a8b33d084d1f04a5cb5d8c33e8d4b220d059d
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-16 17:14:40 +0700

    Membuat fungsi untuk memanggil fungsi getAllMahasiswa dari Mahasiswa Dao

--- app/src/main/java/com/example/mvvmdengandatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/mvvmdengandatabase/repository/RepositoryMhs.kt
@@ -1,8 +1,11 @@
 package com.example.mvvmdengandatabase.repository
 
 import com.example.mvvmdengandatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 //Jika di Dao ada insert di repository juga harus ada insert
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
