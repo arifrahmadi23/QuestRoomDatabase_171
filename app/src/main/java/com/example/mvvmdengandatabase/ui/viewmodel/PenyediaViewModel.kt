f1449debae5557d500569f6e88e79a5d7a68bf7f
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-16 19:24:22 +0700

    Melengkapi PenyediaViewModel

--- app/src/main/java/com/example/mvvmdengandatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mvvmdengandatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.mvvmdengandatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -14,8 +15,26 @@ object PenyediaViewModel{
                 krsApp().containerApp.RepositoryMhs
             )
         }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.RepositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.RepositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.RepositoryMhs
+            )
+        }
     }
 }
 
 fun CreationExtras.krsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
+
